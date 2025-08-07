237806645707aba0d77c3ac0136e302ae8a009cf
Mehedi <mehedi.interface@gmail.com>
2025-08-07 10:33:50 +0300

    javascript statement

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("hello, world!")
+
+document.getElementById('title').innerHTML = "Hello World!"; 
+
+let sum = 150;  
+
+if (sum > 100) {
+    console.log("Expensive");
+} else {
+    console.log("Cheap"); 
+}
